6a872f1e144bd9dbc49a7ac0eeceb2ee8e442272
Ihnatta <annaignatva@gmail.com>
2023-08-10 22:28:18 +0200

    init: start task reverse and finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let x = String( Math.abs(n));
+    
+    let i = 1;
+    let result = '';
+    let y = x.length;
+    
+    while ( i <= y ){
+        result = result + x[y -i];
+        
+        i = i + 1;
+    }
+  return result;
 }
+
